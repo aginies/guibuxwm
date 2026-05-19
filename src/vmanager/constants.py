480180a5998b92e445b2629f935ddb81cd5d8e40
aginies <aginies@suse.com>
2026-05-19 08:37:10 +0200

    update to 3.1.1

--- src/vmanager/constants.py
+++ src/vmanager/constants.py
@@ -47,7 +47,7 @@ class AppInfo:  # pylint: disable=too-few-public-methods
 
     name = "virtui-manager"
     namecase = "VirtUI Manager"
-    version = "3.1.0"
+    version = "3.1.1"
     author = "Antoine Ginies"
 
 
